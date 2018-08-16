21acda1f64626b9f96bd9cf2778f3eca7a3f61d1
rsangole <rahul.sangole@gmail.com>
2018-08-16 06:54:21 -0400

    Starts data utils

--- Solo3/drake_main.R
+++ Solo3/drake_main.R
@@ -1,58 +1,34 @@
 library(drake)
 library(tidyverse)
+library(janitor)
+
 pkgconfig::set_config("drake::strings_in_dots" = "literals") # New file API
 
-data_read <- function(){
-  df <- load('input_data/XYZ_complete_customer_data_frame.RData')
-}
-data_cleanse <- function(){
-  
-}
-data_prep_A <- function(){
-  prep_1()
-  prep_2()
-  #...
-}
-data_prep_B <- function(){
-  prep_1()
-  prep_2()
-  #...
-}
-eda_plots <- function(){
-  
-}
-eda_summaries <- function(){
-  
-}
+source('lib/data_utils.R')
 
-create_plot <- function(data) {
-  ggplot(data, aes(x = Petal.Width, fill = Species)) +
-    geom_histogram(binwidth = 0.25) +
-    theme_gray(20)
-}
-
-# The workflow plan data frame outlines what you are going to do.
-
-plan <- drake_plan(
-  raw_data = readxl::read_excel(file_in("raw_data.xlsx")),
-  data = raw_data %>%
-    mutate(Species = forcats::fct_inorder(Species)) %>%
-    select(-X__1),
-  hist = create_plot(data),
-  fit = lm(Sepal.Width ~ Petal.Width + Species, data),
-  report = rmarkdown::render(
-    knitr_in("report.Rmd"),
-    output_file = file_out("report.html"),
-    quiet = TRUE
-  )
+data_plan <- drake_plan(
+  raw_data = data_read(),
+  df_a = data_prep_A(raw_data)
 )
 
 # Run your work with make().
 
-make(plan)
+make(data_plan)
 
 # See also loadd(), readd(), vis_drake_graph(), and drake_config(). # nolint
 
 # --- # --- #
 
+skim_df <- skimr::skim_to_list(readd(raw_data))
+nzvResults <- caret::nearZeroVar(df, foreach = T, saveMetrics = T)
+nzvResults
+nzvResults$zeroVar %>% tabyl()
+nzvResults %>% dplyr::filter(zeroVar)
 
+# --- # --- #
+
+prep_chr_to_factor <- function(){
+  ADD_TYPE
+  AMEX_REG
+  
+}
---
+++ Solo3/lib/data_utils.R
@@ -0,0 +1,62 @@
+prep_nzv <- function(df, nzv=T){
+  nzvResults <- caret::nearZeroVar(df, foreach = T, saveMetrics = T)
+  zv_cols_to_remove <- rownames(nzvResults[nzvResults$zeroVar,])
+  df[zv_cols_to_remove] <- NULL
+  if(nzv){
+    nzv_cols_to_remove <- rownames(nzvResults[nzvResults$nzv,])
+    df[nzv_cols_to_remove] <- NULL
+  }
+  df
+}
+prep_adultage <- function(df){
+  # Rule: If ADxAGE == U, then ADx_ESTAGE == age, else it's 00
+  # Both are NA at the same time
+  age_cols <- colnames(df) %>% stringr::str_extract(pattern = 'AD[0-9A-Z_]*AGE') %>% na.omit()
+  df$AD2AGE = ifelse(df$AD2AGE=='U', df$AD2_ESTAGE, df$AD2AGE)
+  df$AD3AGE = ifelse(df$AD3AGE=='U', df$AD3_ESTAGE, df$AD3AGE)
+  df$AD4AGE = ifelse(df$AD4AGE=='U', df$AD4_ESTAGE, df$AD4AGE)
+  df$AD5AGE = ifelse(df$AD5AGE=='U', df$AD5_ESTAGE, df$AD5AGE)
+  df$AD6AGE = ifelse(df$AD6AGE=='U', df$AD6_ESTAGE, df$AD6AGE)
+  df$AD7AGE = ifelse(df$AD7AGE=='U', df$AD7_ESTAGE, df$AD7AGE)
+  df$AD8AGE = ifelse(df$AD8AGE=='U', df$AD8_ESTAGE, df$AD8AGE)
+  df$AD2_ESTAGE <- NULL
+  df$AD3_ESTAGE <- NULL
+  df$AD4_ESTAGE <- NULL
+  df$AD5_ESTAGE <- NULL
+  df$AD6_ESTAGE <- NULL
+  df$AD7_ESTAGE <- NULL
+  df$AD8_ESTAGE <- NULL
+  df
+}
+prep_adult_g_r <- function(df){
+  df$ADULT2_G <- factor(df$ADULT2_G, levels = c('M','F','U'))
+  df$ADULT3_G <- factor(df$ADULT3_G, levels = c('M','F','U'))
+  df$ADULT4_G <- factor(df$ADULT4_G, levels = c('M','F','U'))
+  df$ADULT5_G <- factor(df$ADULT5_G, levels = c('M','F','U'))
+  df$ADULT6_G <- factor(df$ADULT6_G, levels = c('M','F','U'))
+  df$ADULT7_G <- factor(df$ADULT7_G, levels = c('M','F','U'))
+  
+  df$ADULT2_R <- factor(df$ADULT2_R, levels = c('H','P','U','W','Y'))
+  df$ADULT3_R <- factor(df$ADULT3_R, levels = c('H','P','U','W','Y'))
+  df$ADULT4_R <- factor(df$ADULT4_R, levels = c('H','P','U','W','Y'))
+  df$ADULT5_R <- factor(df$ADULT5_R, levels = c('H','P','U','W','Y'))
+  df$ADULT6_R <- factor(df$ADULT6_R, levels = c('H','P','U','W','Y'))
+  df$ADULT7_R <- factor(df$ADULT7_R, levels = c('H','P','U','W','Y'))
+  
+  df$ADULT8_G <- NULL
+  df$ADULT8_R <- NULL
+  
+  df
+}
+
+data_read <- function(){
+  read_csv('input_data/complete_cust_data.csv', col_names = T)
+}
+
+
+data_prep_A <- function(df){
+  df %>% 
+    prep_adultage() %>% 
+    prep_adult_g_r() %>% 
+    prep_nzv()
+}
